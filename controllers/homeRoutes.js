9fc75ba9665cdefd6f5eb28b75e5bfc36ed24005
rmay <159880101+rhiannawilson@users.noreply.github.com>
2024-08-05 23:06:53 +1000

    homeRoute.js reviewed and will be tested on Render

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,87 @@
+// const router = require('express').Router();
+// const { Project, User } = require('../models');
+// const withAuth = require('../utils/auth');
+
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const projectData = await Project.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
+
+//     // Serialize data so the template can read it
+//     const projects = projectData.map((project) => project.get({ plain: true }));
+
+//     // Pass serialized data and session flag into template
+//     res.render('homepage', { 
+//       projects, 
+//       logged_in: req.session.logged_in 
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/project/:id', async (req, res) => {
+//   try {
+//     const projectData = await Project.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: User,
+//           attributes: ['name'],
+//         },
+//       ],
+//     });
+
+//     const project = projectData.get({ plain: true });
+
+//     res.render('project', {
+//       ...project,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// // Use withAuth middleware to prevent access to route
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Project }],
+//     });
+
+//     const user = userData.get({ plain: true });
+
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get('/login', (req, res) => {
+//   // If the user is already logged in, redirect the request to another route
+//   if (req.session.logged_in) {
+//     res.redirect('/profile');
+//     return;
+//   }
+
+//   res.render('login');
+// });
+
+// module.exports = router;
+
+
 const router = require('express').Router();
 const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
@@ -23,6 +107,7 @@ router.get('/', async (req, res) => {
       logged_in: req.session.logged_in 
     });
   } catch (err) {
+    console.error(err); // Log the error for debugging
     res.status(500).json(err);
   }
 });
@@ -38,6 +123,11 @@ router.get('/project/:id', async (req, res) => {
       ],
     });
 
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
     const project = projectData.get({ plain: true });
 
     res.render('project', {
@@ -45,6 +135,7 @@ router.get('/project/:id', async (req, res) => {
       logged_in: req.session.logged_in
     });
   } catch (err) {
+    console.error(err); // Log the error for debugging
     res.status(500).json(err);
   }
 });
@@ -58,6 +149,11 @@ router.get('/profile', withAuth, async (req, res) => {
       include: [{ model: Project }],
     });
 
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
     const user = userData.get({ plain: true });
 
     res.render('profile', {
@@ -65,6 +161,7 @@ router.get('/profile', withAuth, async (req, res) => {
       logged_in: true
     });
   } catch (err) {
+    console.error(err); // Log the error for debugging
     res.status(500).json(err);
   }
 });
